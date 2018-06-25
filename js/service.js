03d72d17bcfa8b803e2fea7b938486c9740775ed
lemon <mingxianjun@163.com>
2018-06-25 14:26:51 +0800

    修复bug

--- js/service.js
+++ js/service.js
@@ -54,7 +54,7 @@ app.service('editorUtil',function () {
                 let result = this.gp.parse(content);
                 if(result.success){
                     //校验成功
-                    window._parseResultChange.val = result.res;
+                    window._parseResultChange.val = result.res || {};
                     editorSession.setAnnotations([]);
                 }else {
                     let row = evt.end.row;
